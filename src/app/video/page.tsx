d70bd4c5ddd7e3578cb0ee49ae16e70ef123741f
Anirudhmadhavkulkarni9094 <anirudhkulkarni9094@gmail.com>
2025-11-10 00:59:27 +0530

    fic

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,10 +1,13 @@
 import MultiVideoPage from '@/components/VideoCall'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 function page() {
   return (
     <div>
+      <Suspense fallback={<div>Loading...</div>}>
+
         <MultiVideoPage></MultiVideoPage>
+      </Suspense>
     </div>
   )
 }
